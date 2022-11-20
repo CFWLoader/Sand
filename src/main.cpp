5e6de0e7f91792789eaa6c0d5954a80cf05f1a8c
CFWLoader <cfwloader@gmail.com>
2022-11-20 23:33:07 +0800

    make io test

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <fstream>
 #include "sand/Lexer.h"
 
 using namespace std;
 using namespace Sand;
 
+SandString MakeWinPath(const SandString& RawPath) {
+	static const SandString WinProgramPrefex("..\\..\\..\\");
+	return WinProgramPrefex + RawPath;
+}
+
 int main()
 {
+	ifstream inputFile;
+	SandString ProgramPath(MakeStr("program1.sand"));
+	inputFile.open(MakeWinPath(ProgramPath), ios::in);
+	if (!inputFile.is_open()) {
+		cerr << "Error opening program1.sand" << endl;
+		return 0;
+	}
+	string lineOne;
+	while (!inputFile.eof()) {
+		inputFile >> lineOne;
+		cout << lineOne << endl;
+	}
+	inputFile.close();
+	return 0;
 	// Lexer lexer1(MakeStr("dd"));
-	Lexer lexer1;
-	cout << lexer1.Peek().getLineNumber() << endl;
-	cout << "Hello Sand!" << endl;
+	// Lexer lexer1;
+	// cout << lexer1.Peek().getLineNumber() << endl;
+	// cout << "Hello Sand!" << endl;
 }
