8e47a6275807020d5a074d07f0fa258849155a05
CFWLoader <cfwloader@gmail.com>
2024-07-07 19:25:27 +0800

    actor critic runnable

--- ReinforcementLearner/new_gen/actor_critic/ActorCritic.py
+++ ReinforcementLearner/new_gen/actor_critic/ActorCritic.py
@@ -27,39 +27,63 @@ class Actor(object):
     def learn(self, s, a, td):
         # s, a 用于产生 Gradient ascent 的方向,
         # td 来自 Critic, 用于告诉 Actor 这方向对不对.
-        new_dataform = s[np.newaxis, :]
+        new_dataform = torch.Tensor(s[np.newaxis, :]).cuda()
         probs = self.train_net.forward(new_dataform)
-        log_prob = torch.log(probs)
+        # TF版中是这样访问，应该会有bug
+        log_prob = torch.log(probs[0, a])
         exp_v = log_prob * td
+        loss_val = -exp_v.sum()
+        self.optim.zero_grad()
+        loss_val.backward()
+        self.optim.step()
 
     def choose_action(self, s):
         # 根据 s 选 行为 a
-        pass
+        state_df = torch.Tensor(s[np.newaxis, :]).cuda()
+        probs = self.train_net.forward(state_df).detach().cpu().numpy()
+        return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())
 
 
 class Critic(object):
-    def __init__(self, n_features, lr=0.01):
+    def __init__(self, n_features, lr=0.01, td_gamma=0.9):
+        """
+        @param n_features:
+        @param lr:
+        @param td_gamma: reward discount in TD error
+        """
         # 用 tensorflow 建立 Critic 神经网络,
         # 搭建好训练的 Graph.
-        pass
+        self.td_gamma = td_gamma
+        self.n_hidden = 20
+        self.layer1 = nn.Linear(n_features, self.n_hidden).cuda()
+        self.layer1.weight.data.normal_(.0, .1)
+        nn.init.constant_(self.layer1.bias.data, 0.1)
+
+        self.val_layer = nn.Linear(self.n_hidden, 1).cuda()
+        self.val_layer.weight.data.normal_(.0, .1)
+        nn.init.constant_(self.val_layer.bias.data, 0.1)
+
+        self.train_net = nn.Sequential(
+            self.layer1,
+            nn.ReLU(),
+            self.val_layer
+        )
+
+        self.optim = optim.Adam(self.train_net.parameters(), lr=lr)
 
     def learn(self, s, r, s_):
         # 学习 状态的价值 (state value), 不是行为的价值 (action value),
         # 计算 TD_error = (r + v_) - v,
         # 用 TD_error 评判这一步的行为有没有带来比平时更好的结果,
         # 可以把它看做 Advantage
-        return  # 学习时产生的 TD_error
-
-
-class Critic(object):
-    def __init__(self, sess, n_features, lr=0.01):
-        # 用 tensorflow 建立 Critic 神经网络,
-        # 搭建好训练的 Graph.
-        pass
-
-    def learn(self, s, r, s_):
-        # 学习 状态的价值 (state value), 不是行为的价值 (action value),
-        # 计算 TD_error = (r + v_) - v,
-        # 用 TD_error 评判这一步的行为有没有带来比平时更好的结果,
-        # 可以把它看做 Advantage
-        return  # 学习时产生的 TD_error
+        state_np = s[np.newaxis, :]
+        state, state_ = torch.Tensor(state_np).cuda(), torch.Tensor(s_[np.newaxis, :]).cuda()
+        val_next = self.train_net.forward(state_).detach()
+        val_cur = self.train_net.forward(state)
+        reward = torch.Tensor(np.full(val_next.shape, r)).cuda()
+        td_error = reward + self.td_gamma * val_next - val_cur
+        loss_val = td_error.square()
+        self.optim.zero_grad()
+        loss_val.backward()
+        self.optim.step()
+        return td_error.detach()
---
+++ ReinforcementLearner/new_gen/actor_critic/ac_cartpole.py
@@ -0,0 +1,66 @@
+"""
+Actor-Critic using TD-error as the Advantage, Reinforcement Learning.
+
+The cart pole example. Policy is oscillated.
+
+View more on my tutorial page: https://morvanzhou.github.io/tutorials/
+"""
+
+import numpy as np
+import gym
+from ActorCritic import Actor, Critic
+# from ActorCriticRef import Actor, Critic
+
+np.random.seed(2)
+
+# Superparameters
+OUTPUT_GRAPH = False
+MAX_EPISODE = 3000
+DISPLAY_REWARD_THRESHOLD = 200  # renders environment if total episode reward is greater then this threshold
+MAX_EP_STEPS = 1000   # maximum time step in one episode
+RENDER = False  # rendering wastes time
+GAMMA = 0.9     # reward discount in TD error
+LR_A = 0.001    # learning rate for actor
+LR_C = 0.01     # learning rate for critic
+
+env = gym.make('CartPole-v0')
+env.reset(seed=1)  # reproducible
+env = env.unwrapped
+
+N_F = env.observation_space.shape[0]
+N_A = env.action_space.n
+
+actor = Actor(n_features=N_F, n_actions=N_A, lr=LR_A)
+critic = Critic(n_features=N_F, lr=LR_C, td_gamma=GAMMA)     # we need a good teacher, so the teacher should learn faster than the actor
+
+for i_episode in range(MAX_EPISODE):
+    s, ext_rst_info = env.reset()
+    t = 0
+    track_r = []
+    while True:
+        if RENDER: env.render()
+
+        a = actor.choose_action(s)
+
+        s_, r, done, info, extra_info = env.step(a)
+
+        if done: r = -20
+
+        track_r.append(r)
+
+        td_error = critic.learn(s, r, s_)  # gradient = grad[r + gamma * V(s_) - V(s)]
+        actor.learn(s, a, td_error)     # true_gradient = grad[logPi(s,a) * td_error]
+
+        s = s_
+        t += 1
+
+        if done or t >= MAX_EP_STEPS:
+            ep_rs_sum = sum(track_r)
+
+            if 'running_reward' not in globals():
+                running_reward = ep_rs_sum
+            else:
+                running_reward = running_reward * 0.95 + ep_rs_sum * 0.05
+            if running_reward > DISPLAY_REWARD_THRESHOLD: RENDER = True  # rendering
+            print("episode:", i_episode, "  reward:", int(running_reward))
+            break
