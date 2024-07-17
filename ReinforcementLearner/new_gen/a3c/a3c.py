822644fc8f6a0fb8d8d3a80655e5b2cc8c304dab
CFWLoader <cfwloader@gmail.com>
2024-07-17 10:31:34 +0800

    make stubs

---
+++ ReinforcementLearner/new_gen/a3c/a3c.py
@@ -0,0 +1,10 @@
+import numpy as np
+from torch import nn, optim
+
+
+class AsynchronousAdvantageActorCritic:
+    """
+    多线程版Actor Critic，核心还是Actor Critic，以后有兴趣再来学习并完成
+    """
+    def __init__(self):
+        pass
---
+++ ReinforcementLearner/new_gen/ppo/ppo.py
@@ -0,0 +1,17 @@
+import numpy as np
+from torch import nn, optim, tensor
+
+
+class ProximalPolicyOptimization:
+    def __init__(self, a_dim, s_dim, a_bound):
+        pass
+
+    def update(self, s, a, r):
+        pass
+
+    def choose_action(self, s):
+        pass
+
+    def get_v(self, s):
+        # 算 state value
+        pass
