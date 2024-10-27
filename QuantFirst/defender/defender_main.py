ab31a1a034d2fabea997608a88f9e9a29a4da9a7
Evan <cfwloader@gmail.com>
2024-10-27 23:36:35 +0800

    right way to plot

--- QuantFirst/defender/defender_main.py
+++ QuantFirst/defender/defender_main.py
@@ -1,34 +1,54 @@
+from shutil import which
+
 import adata
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import pandas as pd
+import datetime as dtt
 from defender.signal_hunters.MA20Hunter import MA20Hunter
 
-if __name__ == '__main__':
-    # res_df = adata.stock.info.all_code()
-    # print(res_df)
-
+def manual_way_plot():
+    # 手动设置X轴方法
     res_df = adata.stock.market.get_market(stock_code='300014', k_type=1, start_date='2024-01-01')
     date_col = res_df['trade_date']
-    # datetime_col = mdates.datestr2num(date_col)
-    # exit(0)
+    xdate = [dtt.datetime.strptime(d, '%Y-%m-%d').date() for d in date_col]
     cls_price = res_df['close']
     sig_hunter = MA20Hunter(res_df)
     buyidx, sellidx = sig_hunter.begin_analyze()
     plt.title('Simplest MA20 Trending Trade')
-    # plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    # plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
-    plt.plot(cls_price, label='Close')
-    plt.plot(sig_hunter.ma20trend, label='ma20')
-    plt.vlines(buyidx, ymin=cls_price.min(), ymax=cls_price.max(), ls=':', colors='green', label='Buy in')
-    plt.vlines(sellidx, ymin=cls_price.min(), ymax=cls_price.max(), ls=':', colors='red', label='Sell out')
-    # print(res_df['close'])
-    # res_df['MA5'] = res_df['close'].rolling(5).mean()
-    # res_df['MA20'] = res_df['close'].rolling(20).mean()
-    # plt.plot(res_df['close'])
-    # plt.title('Price MA5, MA20')
-    # plt.ion()
-    # plt.plot()
-    res_df.plot(x="trade_date", y=["close"])
-    # plt.gcf().autofmt_xdate()
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
+    plt.plot(xdate, cls_price, label='Close')
+    plt.plot(xdate, sig_hunter.ma20trend, label='ma20')
+    buydates = [date_col[byidx] for byidx in buyidx]
+    selldates = [date_col[slidx] for slidx in sellidx]
+    plt.vlines(buydates, ymin=cls_price.min(), ymax=cls_price.max(), ls=':', colors='green', label='Buy in')
+    plt.vlines(selldates, ymin=cls_price.min(), ymax=cls_price.max(), ls=':', colors='red', label='Sell out')
+    plt.gcf().autofmt_xdate()
     plt.legend()
     plt.show()
+
+def auto_way_plot():
+    # 自动设置X轴方法
+    fig, ax = plt.subplots()
+    ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(ax.xaxis.get_major_locator()))
+    res_df = adata.stock.market.get_market(stock_code='300014', k_type=1, start_date='2024-01-01')
+    date_col = pd.to_datetime(res_df['trade_date'])
+    cls_price = res_df['close']
+    sig_hunter = MA20Hunter(res_df)
+    buyidx, sellidx = sig_hunter.begin_analyze()
+    buydates = [date_col[byidx] for byidx in buyidx]
+    selldates = [date_col[slidx] for slidx in sellidx]
+    plt.title('Simplest MA20 Trending Trade')
+    plt.plot(date_col, cls_price, label='Close')
+    plt.plot(date_col, sig_hunter.ma20trend, label='ma20')
+    plt.vlines(buydates, ymin=cls_price.min(), ymax=cls_price.max(), ls=':', colors='green', label='Buy in')
+    plt.vlines(selldates, ymin=cls_price.min(), ymax=cls_price.max(), ls=':', colors='red', label='Sell out')
+    plt.gcf().autofmt_xdate()
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    # res_df = adata.stock.info.all_code()
+    manual_way_plot()
+    # auto_way_plot()
