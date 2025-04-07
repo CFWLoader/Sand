01a989adcf07216519c8160ac5d4169be045d8e3
Evan <cfwloader@gmail.com>
2025-04-07 23:01:41 +0800

    cachable demo

--- DeepQuant/section_crawler.py
+++ DeepQuant/section_crawler.py
@@ -9,6 +9,7 @@ from finrl.meta.preprocessor.preprocessors import FeatureEngineer, data_split
 from finrl.meta.env_stock_trading.env_stocktrading import StockTradingEnv
 from finrl.agents.stablebaselines3.models import DRLAgent, MODEL_KWARGS
 from pandas.core.interchange.dataframe_protocol import DataFrame
+from stable_baselines3.a2c.a2c import A2C
 from stable_baselines3.common.logger import configure
 from finrl.meta.data_processor import DataProcessor
 
@@ -34,6 +35,8 @@ from finrl.config import (
     TRADE_END_DATE,
 )
 sys.path.append("../FinRL")
+
+PREDICTION_ROOT='model_predictions'
 
 def get_dataset():
     check_and_make_directories([DATA_SAVE_DIR, TRAINED_MODEL_DIR, TENSORBOARD_LOG_DIR, RESULTS_DIR])
@@ -71,6 +74,8 @@ def get_dataset():
 
     mvo_df = processed_full.sort_values(['date', 'tic'], ignore_index=True)[['date', 'tic', 'close']]
 
+    processed_full.to_csv(os.path.join(DATA_SAVE_DIR, "sample1.csv"), encoding='utf-8')
+
     train = data_split(processed_full, TRAIN_START_DATE, TRAIN_END_DATE)
     trade = data_split(processed_full, TRADE_START_DATE, TRADE_END_DATE)
     print(len(train))
@@ -78,8 +83,20 @@ def get_dataset():
 
     return train, trade
 
-def train_model(train_df):
-    stock_dimension = len(train_df.tic.unique())
+def get_cached_dataset():
+    processed_full = pd.read_csv(os.path.join(DATA_SAVE_DIR, "sample1.csv"), encoding='utf-8')
+    TRAIN_START_DATE = '2009-01-01'
+    TRAIN_END_DATE = '2020-07-01'
+    TRADE_START_DATE = '2020-07-01'
+    TRADE_END_DATE = '2021-10-31'
+    train = data_split(processed_full, TRAIN_START_DATE, TRAIN_END_DATE)
+    trade = data_split(processed_full, TRADE_START_DATE, TRADE_END_DATE)
+    print(len(train))
+    print(len(trade))
+    return train, trade
+
+def get_env_kwargs(input_df):
+    stock_dimension = len(input_df.tic.unique())
     state_space = 1 + 2 * stock_dimension + len(INDICATORS) * stock_dimension
     print(f"Stock Dimension: {stock_dimension}, State Space: {state_space}")
 
@@ -98,6 +115,11 @@ def train_model(train_df):
         "action_space": stock_dimension,
         "reward_scaling": 1e-4
     }
+
+    return env_kwargs
+
+def train_model(train_df) -> A2C:
+    env_kwargs = get_env_kwargs(train_df)
     e_train_gym = StockTradingEnv(df=train_df, **env_kwargs)
 
     env_train, _ = e_train_gym.get_sb_env()
@@ -120,8 +142,21 @@ def train_model(train_df):
                                     tb_log_name='a2c',
                                     total_timesteps=50000) if if_using_a2c else None
 
-import torch
+    return trained_a2c
 
 if __name__ == '__main__':
-    train_df, trade_df = get_dataset()
-    # train_model(train_df)
+    # train_df, trade_df = get_dataset()
+    train_df, trade_df = get_cached_dataset()
+    '''
+    trained_model = train_model(train_df)
+    trained_model.save(os.path.join(TRAINED_MODEL_DIR, 'a2c-sample1.zip'))
+    '''
+    trained_model = A2C.load(os.path.join(TRAINED_MODEL_DIR, 'a2c-sample1.zip'))
+    env_kwargs = get_env_kwargs(train_df)
+    e_trade_gym = StockTradingEnv(df=trade_df, turbulence_threshold=70, risk_indicator_col='vix', **env_kwargs)
+    df_account_value_a2c, df_actions_a2c = DRLAgent.DRL_prediction(
+        model=trained_model,
+        environment=e_trade_gym)
+    check_and_make_directories([os.path.join(PREDICTION_ROOT, "a2c")])
+    df_account_value_a2c.to_csv(os.path.join(PREDICTION_ROOT, "a2c", "action_values-1.csv"))
+    df_actions_a2c.to_csv(os.path.join(PREDICTION_ROOT, "a2c", "actions-1.csv"))
