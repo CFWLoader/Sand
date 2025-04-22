512ee3b717aaa4cad0b2ceba359589a533d16b74
Evan <cfwloader@gmail.com>
2025-04-22 23:56:56 +0800

    runnable with a data

--- DeepQuant/bsm_explorer.py
+++ DeepQuant/bsm_explorer.py
@@ -1,5 +1,11 @@
+from stable_baselines3 import A2C
+
 from src.bigshitmarketdownloader import BigShitMarketDownloader
 from finrl.meta.preprocessor.preprocessors import FeatureEngineer, data_split
+from finrl.meta.env_stock_trading.env_stocktrading import StockTradingEnv
+from finrl.agents.stablebaselines3.models import DRLAgent, MODEL_KWARGS
+from stable_baselines3.common.logger import configure
+from finrl.main import check_and_make_directories
 import pandas as pd
 import os
 import itertools
@@ -17,6 +23,8 @@ from finrl.config import (
     TRADE_START_DATE,
     TRADE_END_DATE,
 )
+
+PREDICTION_ROOT='model_predictions'
 
 CONCERNED_TICKET_LIST=['300014', '600011', '000977']
 
@@ -80,6 +88,67 @@ def get_dataset(use_cache=True):
 # res_df = adata.stock.market.get_market(stock_code='600001', k_type=1, start_date='2020-01-01')
 # res_df = adata.stock.market.get_market(stock_code=stk_code, k_type=1, start_date=start_date)
 
+def get_env_kwargs(input_df):
+    stock_dimension = len(input_df.tic.unique())
+    state_space = 1 + 2 * stock_dimension + len(INDICATORS) * stock_dimension
+    print(f"Stock Dimension: {stock_dimension}, State Space: {state_space}")
+
+    buy_cost_list = sell_cost_list = [0.001] * stock_dimension
+    num_stock_shares = [0] * stock_dimension
+
+    env_kwargs = {
+        "hmax": 100,
+        "initial_amount": 1000000,
+        "num_stock_shares": num_stock_shares,
+        "buy_cost_pct": buy_cost_list,
+        "sell_cost_pct": sell_cost_list,
+        "state_space": state_space,
+        "stock_dim": stock_dimension,
+        "tech_indicator_list": INDICATORS,
+        "action_space": stock_dimension,
+        "reward_scaling": 1e-4
+    }
+
+    return env_kwargs
+
+def train_model(train_df) -> A2C:
+    env_kwargs = get_env_kwargs(train_df)
+    e_train_gym = StockTradingEnv(df=train_df, **env_kwargs)
+
+    env_train, _ = e_train_gym.get_sb_env()
+    print(type(env_train))
+
+    if_using_a2c = True
+
+    agent = DRLAgent(env=env_train)
+    a2c_lparams = {"device": "cuda", "n_steps": 5, "ent_coef": 0.01, "learning_rate": 0.0007}
+    model_a2c = agent.get_model("a2c", model_kwargs=a2c_lparams)
+
+    if if_using_a2c:
+        # set up logger
+        tmp_path = TENSORBOARD_LOG_DIR + '/a2c'
+        new_logger_a2c = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+        # Set new logger
+        model_a2c.set_logger(new_logger_a2c)
+
+    trained_a2c = agent.train_model(model=model_a2c,
+                                    tb_log_name='a2c',
+                                    total_timesteps=50000) if if_using_a2c else None
+
+    return trained_a2c
+
 if __name__ == '__main__':
+    # train_df, trade_df = get_dataset()
     train_df, trade_df = get_dataset()
+    trained_model = train_model(train_df)
+    trained_model.save(os.path.join(TRAINED_MODEL_DIR, 'a2c-sample1.zip'))
+    # trained_model = A2C.load(os.path.join(TRAINED_MODEL_DIR, 'a2c-sample1.zip'))
+    env_kwargs = get_env_kwargs(train_df)
+    e_trade_gym = StockTradingEnv(df=trade_df, turbulence_threshold=70, risk_indicator_col='vix', **env_kwargs)
+    df_account_value_a2c, df_actions_a2c = DRLAgent.DRL_prediction(
+        model=trained_model,
+        environment=e_trade_gym)
+    check_and_make_directories([os.path.join(PREDICTION_ROOT, "a2c")])
+    df_account_value_a2c.to_csv(os.path.join(PREDICTION_ROOT, "a2c", "action_values-1.csv"))
+    df_actions_a2c.to_csv(os.path.join(PREDICTION_ROOT, "a2c", "actions-1.csv"))
 
